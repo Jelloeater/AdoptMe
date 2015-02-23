33457195bd8c24d64b7d170dd3c8757435c4ba84
Jesse <jelloeater@gmail.com>
2015-02-22 23:17:20 -0500

    Need to start from scratch w/ interface, db is good though.

--- app/models.py
+++ app/models.py
@@ -1,118 +1,38 @@
-from flask.ext.appbuilder import Model
-from flask.ext.appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
-
-"""
-You can use the extra Flask-AppBuilder fields and Mixin's
-AuditMixin will add automatic timestamp of created and modified by who
-"""
 import datetime
-from sqlalchemy import Table, Column, Integer, String, Boolean, Numeric, SmallInteger, TIMESTAMP, ForeignKey, \
-    create_engine, MetaData, Date, PrimaryKeyConstraint
-
+from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from flask.ext.appbuilder import Model
 
-
-# http://flask-appbuilder.readthedocs.org/en/latest/quickhowto.html
-
-
-class Payment_Types(Model):
-    payment_type = Column(String(10), primary_key=True)
-    is_credit_card = Column(Boolean, nullable=False)
-
-    def __repr__(self):
-        return self.name
+mindate = datetime.date(datetime.MINYEAR, 1, 1)
 
 
-class US_States(Model):
-    state = Column(String(2), primary_key=True)
-
-    def __repr__(self):
-        return self.name
-
-class Person(Model):
-    person_ID = Column(Integer, primary_key=True)
-    last_name = Column(String(32), nullable=False)
-    first_name = Column(String(32), nullable=False)
-    address = Column(String(32), nullable=False)
-    address2 = Column(String(32))
-    city = Column(String(32), nullable=False)
-    state = Column(ForeignKey('US_States.state'), nullable=False)
-    zip = Column(String(10), nullable=False)
-    home_phone = Column(String(22), nullable=False)
-    work_phone = Column(String(22))
-    mobile_phone = Column(String(22))
-    email = Column(String(50))
-    memo = Column(String(200))
-
-    us_states = relationship('US_States')
+class ContactGroup(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
 
     def __repr__(self):
         return self.name
 
 
-class Payments(Model):
-    payment_ID = Column(Integer, primary_key=True)
-    person_ID = Column(ForeignKey('Person.person_ID'))
-    payment_type = Column(ForeignKey('Payment_Types.payment_type'))
-    timestamp = Column(TIMESTAMP, nullable=False)
-    amount = Column(Numeric, nullable=False)
-
-    person = relationship('Person')
-    payment_types = relationship('Payment_Types')
+class Gender(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique = True, nullable=False)
 
     def __repr__(self):
         return self.name
 
 
-# Adoption = Table('Adoption', metadata,
-# Column('adoption_ID', Integer, primary_key=True),
-# Column('animal_ID', None, ForeignKey('Animal.animal_ID'), nullable=False),
-#                  Column('person_ID', None, ForeignKey('Person.person_ID'), nullable=False),
-#                  Column('is_foster', Boolean, nullable=False),
-#                  Column('timestamp', TIMESTAMP, nullable=False),
-#                  Column('memo', String(200)))
-#
-# Animal = Table('Animal', metadata,
-#                Column('animal_ID', Integer, primary_key=True),
-#                Column('animal_name', String(32), nullable=False),
-#                Column('vet_ID', None, ForeignKey('Vet.vet_ID'), nullable=False),
-#                Column('breed_id', None, ForeignKey('Breed.breed_ID')),
-#                Column('sex', SmallInteger, nullable=False),
-#                # 0 = not known, 1 = male, 2 = female, 9 = not applicable
-#                Column('color', String(32), nullable=False),
-#                Column('spay_nut', Boolean, nullable=False),
-#                Column('spay_nut_date', Date, nullable=False),
-#                Column('dh_date', Date),
-#                Column('hwt_date', Date),
-#                Column('rabies_date', Date),
-#                Column('fvrcp_date', Date),
-#                Column('leuk_date', Date),
-#                Column('stool_date', Date),
-#                Column('stool_result', String(32)),
-#                Column('heart_guard_date', Date),
-#                Column('has_aids', Boolean),
-#                Column('death_date', Date),
-#                Column('memo', String(200)))
-#
-# Breed = Table('Breed', metadata,
-#               Column('breed_ID', Integer, primary_key=True),
-#               Column('type', String(32)),  # Dog, Cat, Etc
-#               Column('breed', String(32)))
-#
-# Vet = Table('Vet', metadata,
-#             Column('vet_ID', Integer, primary_key=True),
-#             Column('vet_name', String(32), nullable=False),
-#             Column('address', String(32), nullable=False),
-#             Column('address2', String(32)),
-#             Column('city', String(32), nullable=False),
-#             Column('state', None, ForeignKey('US_States.state'), nullable=False),
-#             Column('zip', String(10), nullable=False),
-#             Column('work_phone', String(22), nullable=False),
-#             Column('work2_phone', String(22)),
-#             Column('mobile_phone', String(22)),
-#             Column('email', String(50)),
-#             Column('memo', String(200)))
-#
-# metadata.drop_all(engine)
-# metadata.create_all(engine)
+class Contact(Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(150), unique = True, nullable=False)
+    address = Column(String(564))
+    birthday = Column(Date, nullable=True)
+    personal_phone = Column(String(20))
+    personal_celphone = Column(String(20))
+    contact_group_id = Column(Integer, ForeignKey('contact_group.id'), nullable=False)
+    contact_group = relationship("ContactGroup")
+    gender_id = Column(Integer, ForeignKey('gender.id'), nullable=False)
+    gender = relationship("Gender")
+
+    def __repr__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -7,43 +7,102 @@ from flask.ext.babelpkg import lazy_gettext as _
 
 
 from app import db, appbuilder
-from models import Payment_Types, Payments, Person, US_States
+from .models import ContactGroup, Gender, Contact
 
 
-class PersonModelView(ModelView):
-    datamodel = SQLAInterface(Person)
+def fill_gender():
+    try:
+        db.session.add(Gender(name='Male'))
+        db.session.add(Gender(name='Female'))
+        db.session.commit()
+    except:
+        db.session.rollback()
 
 
-    list_columns = ['person_ID', 'last_name', 'first_name', 'address', 'address2', 'city', 'state', 'zip', 'home_phone', 'work_phone', 'mobile_phone', 'email', 'memo']
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
+
+    list_columns = ['name', 'personal_celphone', 'birthday', 'contact_group.name']
 
     base_order = ('name', 'asc')
 
     show_fieldsets = [
-        ('Summary', {'fields': list_columns}),
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
         (
             'Personal Info',
-            {'fields': list_columns, 'expanded': False}),
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
         ]
 
     add_fieldsets = [
-        ('Summary', {'fields': list_columns}),
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
         (
             'Personal Info',
-            {'fields': list_columns, 'expanded': False}),
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
         ]
 
     edit_fieldsets = [
-        ('Summary', {'fields': list_columns}),
+        ('Summary', {'fields': ['name', 'gender', 'contact_group']}),
         (
             'Personal Info',
-            {'fields': list_columns, 'expanded': False}),
+            {'fields': ['address', 'birthday', 'personal_phone', 'personal_celphone'], 'expanded': False}),
         ]
 
 
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+    related_views = [ContactModelView]
 
+
+class ContactChartView(GroupByChartView):
+    datamodel = SQLAInterface(Contact)
+    chart_title = 'Grouped contacts'
+    label_columns = ContactModelView.label_columns
+    chart_type = 'PieChart'
+
+    definitions = [
+        {
+            'group' : 'contact_group',
+            'series' : [(aggregate_count,'contact_group')]
+        },
+        {
+            'group' : 'gender',
+            'series' : [(aggregate_count,'contact_group')]
+        }
+    ]
+
+
+def pretty_month_year(value):
+    return calendar.month_name[value.month] + ' ' + str(value.year)
+
+def pretty_year(value):
+    return str(value.year)
+
+
+class ContactTimeChartView(GroupByChartView):
+    datamodel = SQLAInterface(Contact)
+
+    chart_title = 'Grouped Birth contacts'
+    chart_type = 'AreaChart'
+    label_columns = ContactModelView.label_columns
+    definitions = [
+        {
+            'group' : 'month_year',
+            'formatter': pretty_month_year,
+            'series': [(aggregate_count, 'group')]
+        },
+        {
+            'group': 'year',
+            'formatter': pretty_year,
+            'series': [(aggregate_count, 'group')]
+        }
+    ]
 
 
 db.create_all()
-
+fill_gender()
+appbuilder.add_view(GroupModelView, "List Groups", icon="fa-folder-open-o", category="Contacts", category_icon='fa-envelope')
+appbuilder.add_view(ContactModelView, "List Contacts", icon="fa-envelope", category="Contacts")
 appbuilder.add_separator("Contacts")
+appbuilder.add_view(ContactChartView, "Contacts Chart", icon="fa-dashboard", category="Contacts")
+appbuilder.add_view(ContactTimeChartView, "Contacts Birth Chart", icon="fa-dashboard", category="Contacts")
 
